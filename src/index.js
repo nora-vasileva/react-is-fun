3662dd2da18d5514706d70c74d216aaf77ac841c
Nora Vasileva <nvasileva@Noras-MacBook-Pro.local>
2019-11-24 20:10:11 +0200

    state in constructor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
+let bookList = [
+    {'title': 'Hunger', 'author': 'Roxane Gay', 'pages': 320 },
+    {'title': 'The Sun also rises', 'author': 'Ernest Hemingway', 'pages': 260 },
+    {'title': 'Whute Teeth', 'author': 'Zadie Smith', 'pages': 480 },
+    {'title': 'Cat`s Cradle', 'author': 'Kurt Vonnegut', 'pages': 304 },
+];
+
 const Book = ({author, title, pages}) => {
     return(
         <section>
@@ -11,17 +18,32 @@ const Book = ({author, title, pages}) => {
     )
 }
 
-const Library = () => {
-    return(
-        <div>
-            <Book title='The Sun also rises' author='Ernest Hemingway' pages={260} />
-            <Book title='Whute Teeth' author='Zadie Smith' pages={480} />
-            <Book title='Cat`s Cradle' author='Kurt Vonnegut' pages={304} />
-        </div>
-    )
+class Library extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            open: false
+        }
+        this.toggleOpenClosed = this.toggleOpenClosed.bind(this);
+    }
+    toggleOpenClosed = () => {
+        this.setState({
+            open: !this.state.open
+        })
+    }
+    render() {
+        const {books} = this.props;
+        return(
+            <div>
+                <h1>The Library is {this.state.open ? 'open' : 'closed'}.</h1>
+                <button onClick={this.toggleOpenClosed}>Change</button>
+                { books.map( (book, i) => <Book key={i} title={book.title} author={book.author} pages={book.pages} /> ) }
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(
-    <Library />,
+    <Library books={bookList} />,
     document.getElementById('root')
 )
