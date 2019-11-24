73a9fbd10991bb52a96dcf31e01e4967de61daf2
Nora Vasileva <nvasileva@Noras-MacBook-Pro.local>
2019-11-24 18:54:10 +0200

    react components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,71 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
-var style = {
-    backgroundColor: 'orange',
-    color: 'white',
-    fontFamily: 'Arial'
+let SkiData = {
+    total: 50,
+    powder: 20,
+    backcountry: 10,
+    goal: 100
 }
 
+const getPercent = decimal => {
+    return decimal*100 + '%'
+}
+
+const calcGoalProgress = (total, goal) => {
+    return getPercent(total/goal)
+}
+
+const SkiDayCounter = ({total, backcountry, powder, goal}) => {
+    return (
+        <section>
+                <div>
+                    <p>Total Days: {total}</p>
+                </div>
+                <div>
+                    <p>Powder Days: {powder}</p>
+                </div>
+                <div>
+                    <p>Backcountry Days: {backcountry}</p>
+                </div>
+                <div>
+                    <p>Goal Progress: {calcGoalProgress(total, goal)}</p>
+                </div>
+            </section>
+    )
+}
+
+// class SkiDayCounter extends Component {
+//     getPercent = decimal => {
+//         return decimal * 100 + '%'
+//     }
+
+//     calcGoalProgress = (total, goal) => {
+//         return this.getPercent(total/goal)
+//     }
+
+//     render() {
+//         const {total, powder, backcountry, goal} = this.props;
+//         return (
+//             <section>
+//                 <div>
+//                     <p>Total Days: {total}</p>
+//                 </div>
+//                 <div>
+//                     <p>Powder Days: {powder}</p>
+//                 </div>
+//                 <div>
+//                     <p>Backcountry Days: {backcountry}</p>
+//                 </div>
+//                 <div>
+//                     <p>Goal Progress: {this.calcGoalProgress(total, goal)}</p>
+//                 </div>
+//             </section>
+//         )
+//     }
+// }
+
 ReactDOM.render(
-    <div style={style}>
-        <h1 id="heading-element">Hello World</h1>
-        <p>I am glad you are here.</p>
-    </div>,
+    <SkiDayCounter total={SkiData.total} powder={SkiData.powder} backcountry={SkiData.backcountry} goal={SkiData.goal} />,
     document.getElementById('root')
 )
